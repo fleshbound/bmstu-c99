d457600d978d1590755085d49a4d9a91c28aa9c6
Avdeykina Valeriya <avp21u792@student.bmstu.ru>
2022-03-03 22:10:39 +0300

    lab_01_09_01/: lab_01_09_01.c was added.
    lab_01_09_01/: positive (3) and negative (3) tests, test scripts and readme.md file were added.

---
+++ lab_01_09_01/func_tests/scripts/test_pos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Запуск: ./имя_скрипта.sh номер_лабораторной_NN
+# Скрипт должен находиться в папке "scripts"
+
+if [ -z "$1" ]; then
+	echo "Invalid key(s) (number of lab)"
+	exit 1
+else
+	code="$1"
+	main_code="../../lab_01_""$code""_01.c"
+	tests_dir="../data"
+	pos_in_files=$tests_dir"/pos_**_in.txt"
+
+	gcc -std=c99 -Wall -Werror -Wpedantic -o "temp.exe" "$main_code" -lm
+	
+	i=0
+	q_neg=0
+	for file in $pos_in_files; do
+		i=$(( i + 1 ))
+		
+		numbers="$(grep -o "[+,-]*[0-9][0-9]*\.*[0-9]*" "$file")"
+		
+		./sub_test_pos.sh "$i" "$file"
+		
+		if [ $? -eq 0 ]; then
+			echo "Test $i: OK"
+		else
+			q_neg=$(( q_neg + 1 ))
+			echo "Test $i: FAILED"
+		fi
+	done
+
+	echo -e "\nFailed $q_neg test(s) of total $i. Quiting..."
+	
+	rm "temp.exe"
+	
+	exit 0	
+fi
---
+++ lab_01_09_01/lab_01_09_01.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ERR_OK 0
+#define ERR_VALUE 1
+#define ERR_NEG 2
+
+// Input check
+
+int check_scanf(float *num)
+{
+	printf("Enter x: ");
+	int check = scanf("%f", num);
+
+	if (check != 1)
+	{
+		printf("Error: Invalid value.\n");
+		exit(ERR_VALUE);
+	}
+	else
+	{
+		if (*num < 0)
+		{
+			return ERR_NEG;
+		}
+		else
+		{
+			return ERR_OK;
+		}
+	}
+}
+
+
+int main(void)
+{
+	int check = 0, i = 1, flag = 0;
+	float x, sum = 0, g_x = 0;
+
+	printf("Enter float positive values; negative value will end the entering\nand show the result of calculating.\n");
+	while (check == ERR_OK)
+	{
+		check = check_scanf(&x);
+		if (x >= 0)
+		{
+			flag = 1;
+			sum += sqrt(x / i);
+			i++;
+		}
+	}
+
+	if (flag == 0)
+	{
+		printf("Error: No value has been entered.\n");
+		return ERR_VALUE;
+	}
+
+	g_x = sin(sum);
+	printf("g(x): %f \n", g_x);
+
+	return 0;
+}
