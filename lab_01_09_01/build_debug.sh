5973747aa6457c401b0ac1a0e5056bd94f015aeb
Valeriya Avdeykina <avp21u792@student.bmstu.ru>
2022-03-15 16:38:31 +0300

    Debug scripts were cleaned (spaces to tabs).

--- lab_01_09_01/build_debug.sh
+++ lab_01_09_01/build_debug.sh
@@ -8,30 +8,30 @@ ERR_EXIST=3
 
 check_reg="^*.c"
 if [ -z "$2" ]; then
-        echo "$0: Wrong number of arguments (expected 2 or more, got $#)"
-        exit $ERR_ARGS
+	echo "$0: Wrong number of arguments (expected 2 or more, got $#)"
+	exit $ERR_ARGS
 elif ( echo "$1" | grep -Eq "^*.exe$"  ); then
-        exe_name="$1"
-        shift
-        files="$@"
+	exe_name="$1"
+	shift
+	files="$@"
 
-        for file in $files; do
-                if ( echo "$file" | grep -Eq "$check_reg" ); then
-                        if [ ! \( -f "$file" \) ]; then
-                                echo "$0: File $file doesn't exist"
-                                exit $ERR_EXIST
-                        fi
-                else
-                        echo "$0: Wrong object file(s) extension"
-                        exit $ERR_EXT
-                fi
-        done
+	for file in $files; do
+		if ( echo "$file" | grep -Eq "$check_reg" ); then
+			if [ ! \( -f "$file" \) ]; then
+				echo "$0: File $file doesn't exist"
+				exit $ERR_EXIST
+			fi
+		else
+			echo "$0: Wrong object file(s) extension"
+			exit $ERR_EXT
+		fi
+	done
 
-        gcc -std=c99 -Wall -Werror -Wpedantic -g3 -c "$@"
-        gcc -o "$exe_name" "*.o"
-        echo "[$0] Build debug was created"
-        exit 0
+	gcc -std=c99 -Wall -Werror -Wpedantic -g3 -c "$@"
+	gcc -o "$exe_name" "*.o"
+	echo "[$0] Build debug was created"
+	exit 0
 else
-        echo "$0: Wrong executable file extension (.exe expected)"
-        exit $ERR_EXT
+	echo "$0: Wrong executable file extension (.exe expected)"
+	exit $ERR_EXT
 fi
