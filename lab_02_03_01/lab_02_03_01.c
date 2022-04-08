aa35ae6edb090b0285348cb2cc1be465777e3256
Valeriya Avdeykina <avp21u792@student.bmstu.ru>
2022-04-09 00:38:22 +0300

    Some fixes in 3 and 2 were added.

--- lab_02_03_01/lab_02_03_01.c
+++ lab_02_03_01/lab_02_03_01.c
@@ -1,136 +1,122 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 #define N 10
 #define N_MAX 20
 #define ERR_VALUE 1
 #define ERR_SIZE 2
 #define ERR_NONE 101
+#define NUMBER_OF_INPUT_INT 1
+
+
+// Вставка элемента в массив на позицию pos
+void insert_element(int *const a, const size_t size, const size_t pos, const int element)
+{
+    for (int i = size - 1; i > pos; i--) 
+        a[i] = a[i - 1];
+    a[pos] = element;
+}
 
 
 // Вставка чисел Фибоначчи в массив
-int insert_fibo(int *a, const size_t size)
+int insert_fibo(int *const a, const size_t size)
 {
-	int temp[N_MAX];
-	int q_fibo = 0;
-	int last_fibo = 1, curr_fibo = 0;
-	int temp_fibo;
+    int last_fibo = 1, curr_fibo = 0, temp_fibo;
+    
+    size_t curr_size = size;
+    size_t i = 0;
+    while (i < curr_size)
+    {
+        if (a[i] % 3 == 0)
+        {
+            curr_size++;
+            insert_element(a, curr_size, i + 1, curr_fibo);
+            i++;
 
-	for (int i = 0; i < size; i++)
-	{
-		temp[i + q_fibo] = a[i];
+            temp_fibo = curr_fibo;
+            curr_fibo = last_fibo + temp_fibo;
+            last_fibo = temp_fibo;
+        }
+        i++;
+    }
 
-		if (a[i] % 3 == 0)
-		{
-			temp[i + q_fibo + 1] = curr_fibo;
-
-			q_fibo += 1;
-
-			temp_fibo = curr_fibo;
-			curr_fibo = last_fibo + temp_fibo;
-			last_fibo = temp_fibo;
-		}
-	}
-
-	int curr_size = size + q_fibo;
-
-	for (int i = 0; i < curr_size; i++)
-	{
-		a[i] = temp[i];
-	}
-
-	return curr_size;
+    return curr_size;
 }
 
+
 // Ввод массива с проверкой на корректность
-int input_array(int *a, const size_t size)
+int input_array(int *const a, const size_t size)
 {
-	printf("Enter integers:\n");
-	
-	for (int i = 0; i < size; i++)
-	{
-		int element;
-		int check = scanf("%d", &element);
-		
-		if (check != 1)
-		{
-			printf("Error: Not an integer\n");
-			
-			return ERR_VALUE;
-		}
-		
-		a[i] = element;
-	}
-	
-	return EXIT_SUCCESS;
+    printf("Enter integers:\n");
+    
+    for (int i = 0; i < size; i++)
+    {
+        int element;
+        int check = scanf("%d", &element);
+        
+        if (check != NUMBER_OF_INPUT_INT)
+        {
+            printf("Error: Not an integer\n");
+            
+            return ERR_VALUE;
+        }
+        
+        a[i] = element;
+    }
+    
+    return EXIT_SUCCESS;
 }
 
 
 // Ввод размера массива
-int input_size(size_t *size)
+int input_size(size_t *const size)
 {
-	size_t n;
+    size_t n;
 
-	printf("Enter the size of array: ");
-	int check = scanf("%lu", &n);
-	
-	if (1 != check) 
-	{
-		printf("Size must be integer\n");
-	
-		return ERR_VALUE;
-	}
-	
-	if ((10 < n) || (0 >= n))
-	{
-		printf("Size must be positive and less or equal to ten\n");
-		
-		return ERR_SIZE;
-	}
-	
-	*size = n;
-
-	return EXIT_SUCCESS;
+    printf("Enter the size of array: ");
+    int check = scanf("%lu", &n);
+    
+    if (check != NUMBER_OF_INPUT_INT) 
+    {
+        printf("Size must be integer\n");
+        return ERR_VALUE;
+    }
+    
+    if ((n > 10) || (n <= 0))
+    {
+        printf("Size must be positive and less or equal to ten\n");
+        return ERR_SIZE;
+    }
+    
+    *size = n;
+    return EXIT_SUCCESS;
 }
 
 
 // Вывод массива
 void output_array(const int *a, const size_t size)
 {
-	for (int i = 0; i < size; i++)
-	{
-		printf("%d ", a[i]);
-	}
-
-	printf("\n");
+    for (int i = 0; i < size; i++) 
+        printf("%d%s", a[i], (i == size - 1) ? "\n" : " ");
 }
 
 int main(void)
 {
-	size_t size;
-	int check_size = input_size(&size);
-	
-	if (check_size == ERR_VALUE)
-	{
-		return ERR_VALUE;
-	}
+    size_t size;
+    int error_size = input_size(&size);
     
-	if (check_size == ERR_SIZE)
-	{
-		return ERR_SIZE;
-	}
-	
-	int array[N_MAX];
-	int check_array = input_array(array, size);
-	
-	if (check_array == ERR_VALUE)
-	{
-		return ERR_VALUE;
-	}
-	
-	int new_size = insert_fibo(array, size);
+    if (error_size != EXIT_SUCCESS)
+        return error_size;
+    
+    int array[N_MAX];
+    int error_array = input_array(array, size);
+    
+    if (error_array != EXIT_SUCCESS) 
+        return error_array;
+    
+    int new_size = insert_fibo(array, size);
+    output_array(array, new_size);
 
-	output_array(array, new_size);
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
