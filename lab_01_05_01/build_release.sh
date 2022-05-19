992c46730a3bc598cfb66cbe74d67b1984b21c9b
Avdeykina Valeriya <avp21u792@student.bmstu.ru>
2022-05-19 21:36:30 +0300

    Build scripts were updated.

--- lab_01_05_01/build_release.sh
+++ lab_01_05_01/build_release.sh
@@ -2,16 +2,27 @@
 
 ERR_EXIST=1
  
-files=$( find . -regextype posix-egrep -regex "\.*/[a-zA-Z_0-9]+\.c" )
+c_files=$( find . -regextype posix-egrep -regex "\.*/[a-zA-Z_0-9]+\.c" )
 
-if [ -z "$files" ]; then
+if [ -z "$c_files" ]; then
     echo "$0: No .c files were found."
 
     exit $ERR_EXIST
 fi
 
-gcc -std=c99 -Wall -Werror -Wextra -Wfloat-equal -Wfloat-conversion -Wvla -O3 -g0 -c main.c
-gcc -o app.exe ./*.o -lm
+for file in $c_files; do
+	gcc -std=c99 -Wall -Werror -Wextra -Wfloat-equal -Wfloat-conversion -Wvla -O3 -g0 -c "$file"
+done
+
+o_files=$( find . -regextype posix-egrep -regex "\.*/[a-zA-Z_0-9]+\.o" ! -name "main.o" | cut -f 2 -d '/' )
+f=()
+i=0
+for file in $o_files; do
+    f[i]="$file"
+    i=$(( i + 1 ))
+done
+
+gcc -o app.exe main.o "${f[@]}" -lm
 
 if [ -s app.exe ]; then
     echo "[$0] Release build was created"
